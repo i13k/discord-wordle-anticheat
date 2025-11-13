5c4f4fd3d7459d252b17046c3c6ca52154b74cd3
Konrad Goliński <93205811+i13k@users.noreply.github.com>
2025-11-13 16:58:52 +0100

    Update wordle_api.py

--- wordle_api.py
+++ wordle_api.py
@@ -1,19 +1,16 @@
 import aiohttp
 import datetime
 
-async def fetch_answer_from_api() -> dict:
+async def fetch_answer_from_api() -> str:
     today_date_str = datetime.datetime.now().strftime("%Y-%m-%d")
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://www.nytimes.com/svc/wordle/v2/{today_date_str}.json") as r:
             if r.status == 200:
-                return await r.json()
+                return (await r.json()).get("solution", "")
             raise Exception("Failed to fetch Wordle answer from API")
 
-async def get_today_answer(answer_cache) -> str:
-    if answer_cache["date"] == datetime.date.today():
-        return answer_cache["word"]
-    res = await fetch_answer_from_api()
-    word = res.get("solution", "").lower()
-    answer_cache["word"] = word
-    answer_cache["date"] = datetime.date.today()
-    return word
+async def update_answer_cache(answer_cache) -> str:
+    today = datetime.date.today()
+    if answer_cache[0] == today:
+        return answer_cache
+    return (today, (await fetch_answer_from_api()))
