209d2492c7f2d8145c94e29e888bc5300904e1c8
Konrad Goliński <93205811+i13k@users.noreply.github.com>
2025-11-13 18:21:54 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ async def on_message(message: discord.Message) -> None:
     guild = await get_guild(message.guild.id)
     if not guild.enabled:
         return
-    message_text = re.sub(r"(:regional_indicator_[A-Za-z]:)|[^A-Za-z]", "", message.content).lower()
+    message_text = re.sub(r"(regional_indicator_)|[^A-Za-z]", "", message.content).lower()
     if client.answer_cache[1] in message_text:
         await message.delete()
         await message.channel.send(f":warning: {message.author.mention}, your message has been deleted because it contained today's Wordle answer.", silent=True)
