89fa8b6383c3e67aaa0f39c206138f281d413569
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2025-11-25 19:46:02 +0100

    Added `/about` command

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ import database
 from wordle_api import update_answer_cache
 from guild_functions import get_guild, change_enabled
 
+from __version__ import __version__
+
 CONFIG_PATH = "config.yaml"
 
 def load_config() -> dict:
@@ -29,39 +31,52 @@ class Client(discord.Client):
 
 client = Client()
 
-@client.tree.command(name="enable", description="Enable Wordle anti-cheat")
-async def enable_anticheat(interaction: discord.Interaction) -> None:
-    await interaction.response.defer(ephemeral=True)
-    if not interaction.user.guild_permissions.administrator:
-        await interaction.followup.send(":lock: **Access denied!** This command is only for server administrators.")
-        return
-    guild = await get_guild(interaction.guild.id)
-    if guild.enabled:
-        await interaction.followup.send(":information_source: Wordle anti-cheat is already **enabled** in this server.")
-        return
-    await change_enabled(guild, True)
-    await interaction.followup.send(":white_check_mark: Wordle anti-cheat **successfully enabled**.")
+# @client.tree.command(name="enable", description="Enable Wordle anti-cheat")
+# async def enable_anticheat(interaction: discord.Interaction) -> None:
+#     await interaction.response.defer(ephemeral=True)
+#     if not interaction.user.guild_permissions.administrator:
+#         await interaction.followup.send(":lock: **Access denied!** This command is only for server administrators.")
+#         return
+#     guild = await get_guild(interaction.guild.id)
+#     if guild.enabled:
+#         await interaction.followup.send(":information_source: Wordle anti-cheat is already **enabled** in this server.")
+#         return
+#     await change_enabled(guild, True)
+#     await interaction.followup.send(":white_check_mark: Wordle anti-cheat **successfully enabled**.")
 
-@client.tree.command(name="disable", description="Disable Wordle anti-cheat")
-async def disable_anticheat(interaction: discord.Interaction) -> None:
-    await interaction.response.defer(ephemeral=True)
-    if not interaction.user.guild_permissions.administrator:
-        await interaction.followup.send(":lock: **Access denied!** This command is only for server administrators.")
-        return
-    guild = await get_guild(interaction.guild.id)
-    if not guild.enabled:
-        await interaction.followup.send(":information_source: Wordle anti-cheat is already **disabled** in this server.")
-        return
-    await change_enabled(guild, False)
-    await interaction.followup.send(":negative_squared_cross_mark: Wordle anti-cheat **successfully disabled**.")
+# @client.tree.command(name="disable", description="Disable Wordle anti-cheat")
+# async def disable_anticheat(interaction: discord.Interaction) -> None:
+#     await interaction.response.defer(ephemeral=True)
+#     if not interaction.user.guild_permissions.administrator:
+#         await interaction.followup.send(":lock: **Access denied!** This command is only for server administrators.")
+#         return
+#     guild = await get_guild(interaction.guild.id)
+#     if not guild.enabled:
+#         await interaction.followup.send(":information_source: Wordle anti-cheat is already **disabled** in this server.")
+#         return
+#     await change_enabled(guild, False)
+#     await interaction.followup.send(":negative_squared_cross_mark: Wordle anti-cheat **successfully disabled**.")
 
-@client.tree.command(name="status", description="Get status of Wordle anti-cheat")
+# @client.tree.command(name="status", description="Get status of Wordle anti-cheat")
+# async def anticheat_status(interaction: discord.Interaction) -> None:
+#     await interaction.response.defer(ephemeral=True)
+#     guild = await get_guild(interaction.guild.id)
+#     message = f"Wordle anti-cheat is currently **{':white_check_mark: enabled' if guild.enabled else ':negative_squared_cross_mark: disabled'}** in this server.\n"
+#     message += f"Use `{'/enable' if not guild.enabled else '/disable'}` to {'enable' if not guild.enabled else 'disable'} it."
+#     await interaction.followup.send(message)
+
+@client.tree.command(name="about", description="About the bot")
 async def anticheat_status(interaction: discord.Interaction) -> None:
     await interaction.response.defer(ephemeral=True)
-    guild = await get_guild(interaction.guild.id)
-    message = f"Wordle anti-cheat is currently **{':white_check_mark: enabled' if guild.enabled else ':negative_squared_cross_mark: disabled'}** in this server.\n"
-    message += f"Use `{'/enable' if not guild.enabled else '/disable'}` to {'enable' if not guild.enabled else 'disable'} it."
-    await interaction.followup.send(message)
+    embed = discord.Embed(
+        title="Wordle Anti-cheat",
+        description="**Anti-cheat** bot for Wordle activity in Discord.\nBot deletes messages that contain Wordle answers.",
+        color=0x0000dd
+    )
+    embed.add_field(name="Version", value=f"`{__version__}`", inline=False)
+    embed.add_field(name="Source code", value="[GitHub Repository](https://github.com/bartekl1/discord-wordle-anticheat)", inline=False)
+    embed.add_field(name="License", value="GNU Affero General Public License v3.0", inline=False)
+    await interaction.followup.send(embed=embed)
 
 @client.event
 async def on_message(message: discord.Message) -> None:
