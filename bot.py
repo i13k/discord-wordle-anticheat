13da9f9270e4d6e4e3815569946f4193debd321d
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2025-11-25 17:54:36 +0100

    Added option to replace diacritics when checking message

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord import app_commands
+from unidecode import unidecode
 import yaml
 import asyncio
 import re
@@ -70,15 +71,13 @@ async def on_message(message: discord.Message) -> None:
     guild = await get_guild(message.guild.id)
     if not guild.enabled:
         return
-    
-    # TODO: Move to settings
-    detection_after_chars_removed = True
-    reversed_detection = True
 
-    message_text = re.sub(r"[^A-Za-z]", "", message.content).lower() if detection_after_chars_removed else message.content.lower()
-    
+    message_text = message.content.lower()
+    message_text = unidecode(message_text) if guild.replace_diacritics else message_text
+    message_text = re.sub(r"[^A-Za-z]", "", message_text).lower() if guild.remove_not_letters else message_text
+
     if client.answer_cache[1] in message_text or \
-       (client.answer_cache[1][::-1] in message_text and reversed_detection):
+       (client.answer_cache[1][::-1] in message_text and guild.reversed_detection):
         await message.delete()
         await message.channel.send(f":warning: {message.author.mention}, your message has been deleted because it contained today's Wordle answer.", silent=True)
 
--- models.py
+++ models.py
@@ -9,6 +9,9 @@ class Guild(Base):
     id = Column(Integer, primary_key=True)
     discord_id = Column(Integer, unique=True, nullable=False)
     enabled = Column(Boolean, default=False, nullable=False)
+    # replace_diacritics = Column(Boolean, default=False, nullable=False)
+    # remove_not_letters = Column(Boolean, default=False, nullable=False)
+    # reversed_detection = Column(Boolean, default=False, nullable=False)
 
     def __repr__(self) -> str:
         return f"<Guild id={self.id} discord_id={self.discord_id} enabled={self.enabled}>"
