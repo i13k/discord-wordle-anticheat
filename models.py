6b93121687605263b8f13d0432c806d3cd534f84
Konrad Goliński <93205811+i13k@users.noreply.github.com>
2025-11-30 14:45:43 +0100

    Update models.py

--- models.py
+++ models.py
@@ -13,6 +13,7 @@ class Guild(Base):
     remove_not_letters = Column(Boolean, nullable=False, server_default="0")
     reversed_detection = Column(Boolean, nullable=False, server_default="0")
     send_messages = Column(Boolean, nullable=False, server_default="1")
+    delete_wordle_messages = Column(Boolean, nullable=False, server_default="0")
 
     def __repr__(self) -> str:
         return f"<Guild id={self.id} discord_id={self.discord_id} enabled={self.enabled}>"
